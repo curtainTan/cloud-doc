8f27c00fbfb43679329037245bb3b3bb93442eb2
curtainTan <1948750473@qq.com>
2021-02-22 21:30:00 +0800

    feat: feat: finish auto focus

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-3 left-panel">
+        <div className="col-4 left-panel">
           <FileSearch title="我的云文档" onFileSearch={() => {}} />
         </div>
-        <div className="col-9 bg-primary right-panel">
+        <div className="col-8 bg-primary right-panel">
           <h2>我是右侧</h2>
         </div>
       </div>
--- src/components/fileSearch.js
+++ src/components/fileSearch.js
@@ -1,8 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const FileSearch = ({ title, onFileSearch }) => {
   const [inputActive, setInputActive] = useState(false);
   const [value, setValue] = useState('');
+  const closeSearch = (e = new Event()) => {
+    e.preventDefault();
+    setInputActive(false);
+    setValue('');
+  };
+  const node = useRef(null);
+  useEffect(() => {
+    inputActive && node.current.focus();
+  }, [inputActive]);
+
+  useEffect(() => {
+    const handleInputEvent = e => {
+      console.log('--------keycode---------');
+      const { keyCode } = e;
+      if (keyCode === 13 && inputActive) {
+        // onFileSearch(value);
+        console.log('-------value---------', value);
+      }
+      if (keyCode === 27 && inputActive) {
+        closeSearch(e);
+      }
+    };
+    document.addEventListener('keyup', handleInputEvent);
+    return () => {
+      document.removeEventListener('keyup', handleInputEvent);
+    };
+  });
 
   return (
     <div className="alert alert-primary">
@@ -23,16 +50,12 @@ const FileSearch = ({ title, onFileSearch }) => {
           <input
             className="form-control col-8"
             value={value}
+            ref={node}
             onChange={e => {
               setValue(e.target.value);
             }}
           />
-          <button
-            className="btn btn-primary col-4"
-            onClick={() => {
-              setInputActive(false);
-            }}
-          >
+          <button className="btn btn-primary col-4" onClick={closeSearch}>
             关闭
           </button>
         </div>
