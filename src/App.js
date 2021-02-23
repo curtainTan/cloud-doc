1332208c4a43ab4a6ef17db4a9153abd78995651
curtainTan <1948750473@qq.com>
2021-02-23 17:53:25 +0800

    fix: UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { faPlus, faFileImport } from '@fortawesome/free-solid-svg-icons';
 import SimpleMDE from 'react-simplemde-editor';
 
@@ -8,23 +8,34 @@ import FileList from './components/fileList';
 import BottomBtn from './components/BottomBtn';
 import TabList from './components/tab/index';
 
-import './components/fileSearch.css';
+import './app.css';
 
 function App() {
+  const [files, setFiles] = useState(filesData);
+  const [activeFileID, setActiveFileId] = useState('');
+  const [openedFileIDs, setOpenedFileIDs] = useState([]);
+  const [unsavedFileIDs, setUnsavedFileIDs] = useState([]);
+
+  const activeFile = files.find(file => file.id === activeFileID);
+
+  const openedFiles = openedFileIDs.map(openID => {
+    return files.find(file => file.id === openID);
+  });
+
   return (
     <div className="container-fluid px-0">
       <div className="row no-gutters">
         <div className="col-4 left-panel">
           <FileSearch onFileSearch={() => {}} />
           <FileList
-            files={filesData}
+            files={files}
             onFileDelete={() => {}}
             onFileClick={() => {}}
             onSaveEdit={(id, newTitle) => {
               console.log('-----save--', id, newTitle);
             }}
           />
-          <div className="row no-gutters">
+          <div className="row no-gutters button-group">
             <div className="col">
               <BottomBtn text="新建" colorClass="btn-primary" icon={faPlus} />
             </div>
@@ -34,26 +45,31 @@ function App() {
           </div>
         </div>
         <div className="col-8 right-panel">
-          <TabList
-            files={filesData}
-            activedId="1"
-            unsaveIds={['1']}
-            onTabClick={id => {
-              console.log('---id--', id);
-            }}
-            onCloseTab={id => {
-              console.log('delete----', id);
-            }}
-          />
-          <SimpleMDE
-            value={filesData[1].body}
-            onChange={val => {
-              console.log(val);
-            }}
-            options={{
-              minHeight: '515px',
-            }}
-          />
+          {!activeFile && <div className="start-page">选择或者创建新的 MarkDown 文档</div>}
+          {activeFile && (
+            <>
+              <TabList
+                files={openedFiles}
+                activedId={activeFileID}
+                unsaveIds={unsavedFileIDs}
+                onTabClick={id => {
+                  console.log('---id--', id);
+                }}
+                onCloseTab={id => {
+                  console.log('delete----', id);
+                }}
+              />
+              <SimpleMDE
+                value={activeFile && activeFile.body}
+                onChange={val => {
+                  console.log(val);
+                }}
+                options={{
+                  minHeight: '515px',
+                }}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
